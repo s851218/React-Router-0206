f42aaae035225f43d7c3f16e3dc92f0ebace6ab7
s851218 <sin0981802858@gmail.com>
2025-02-07 18:12:49 +0800

    main.jsx BrowserRouter 改成 HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   // repositoryName 要改成自己的 Repo 名稱
-  base: process.env.NODE_ENV === "production" ? "/repositoryName/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/React-Router-0206/" : "/",
   plugins: [react()],
 });
